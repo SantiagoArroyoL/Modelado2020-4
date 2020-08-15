a1dfb0f4891c24fe45c8214a346dcd053633e207
sgonzalezd <sgonzalezd@ciencias.unam.mx>
2020-08-15 07:49:54 -0500

    Agregado el paso por referencia y validacion de costo en predeterminados

--- Factory/src/ciencias/unam/modelado/Prueba.java
+++ Factory/src/ciencias/unam/modelado/Prueba.java
@@ -10,21 +10,23 @@ import ciencias.unam.modelado.componentes.llantas.FabricaLlanta;
 import ciencias.unam.modelado.componentes.motores.FabricaMotor;
 
 public class Prueba {
+
     public static void main(String[] args) {
-        
+
         System.out.println("Bienvenido a la tienda de automóviles personalizados MaxMad");
-        System.out.println("Para poder comprar debes invertir una cantidad inicial,\n si te sobra dinero te lo notificaremos al final.");
+        System.out.println(
+                "Para poder comprar debes invertir una cantidad inicial,\n si te sobra dinero te lo notificaremos al final.");
         System.out.println("¿Cuanto deseas invertir en tu nuevo coche?");
-        System.out.println("1- $1000 ");
-        System.out.println("2- $2000 ");
-        System.out.println("3- $20000 ");
-        double dinero_inicial = new double[]{1000, 2000, 20000}[lee(1,3)-1]; 
+        System.out.println("1- $8000 ");
+        System.out.println("2- $4000 ");
+        System.out.println("3- $3000 ");
+        ReferenciaNumeroDoble dinero_inicial = new ReferenciaNumeroDoble(new double[] { 3000, 4000, 20000 }[lee(1, 3) - 1]);
 
         System.out.println("Deseas armar tu carro o ver alguna de las opciones predeterminadas");
         System.out.println("1- Opciones predeterminadas ");
         System.out.println("2- Armar mi carro ");
         Coche coche = null;
-        switch (lee(1,2)) {
+        switch (lee(1, 2)) {
             case 1:
                 coche = menuPredifinido(dinero_inicial);
                 break;
@@ -38,68 +40,83 @@ public class Prueba {
         System.out.println("Ataque: " + coche.getAtaque());
         System.out.println("Defensa: " + coche.getDefensa());
         System.out.println("Velocidad: " + coche.getVelocidad());
+        System.out.println("Tu cambio, no te lo daremos, es MaxMad, pero te habian sobrado: " + dinero_inicial.getValue());
         System.out.println("SUERTE EN EL APOCALIPSIS!");
     }
 
-    private static Coche menuPersonalizado(double dineroInicial){
+    private static Coche menuPersonalizado(ReferenciaNumeroDoble dineroInicial) {
         int seleccion;
         System.out.println("Escoge unas llantas");
         for (int i = 0; i < FabricaLlanta.catalogo.length; i++) {
-            System.out.println((i+1)+".-" + FabricaLlanta.catalogo[i] +
-                    " Costo: " + FabricaLlanta.getllanta(FabricaLlanta.catalogo[i]).getCosto());
+            System.out.println((i + 1) + ".-" + FabricaLlanta.catalogo[i] + " Costo: "
+                    + FabricaLlanta.getllanta(FabricaLlanta.catalogo[i]).getCosto());
         }
-        seleccion = lee(1, FabricaLlanta.catalogo.length)-1;
-        dineroInicial -=  FabricaLlanta.getllanta(FabricaLlanta.catalogo[seleccion]).getCosto();
+        seleccion = lee(1, FabricaLlanta.catalogo.length) - 1;
+        dineroInicial.setValue(
+                dineroInicial.getValue() - FabricaLlanta.getllanta(FabricaLlanta.catalogo[seleccion]).getCosto());
         String llanta = FabricaLlanta.catalogo[seleccion];
 
         System.out.println("Escoge una arma");
         for (int i = 0; i < FabricaArma.catalogo.length; i++) {
-            System.out.println((i+1)+".-" + FabricaArma.catalogo[i] +
-                    " Costo: " + FabricaArma.getArma(FabricaArma.catalogo[i]).getCosto());
+            System.out.println((i + 1) + ".-" + FabricaArma.catalogo[i] + " Costo: "
+                    + FabricaArma.getArma(FabricaArma.catalogo[i]).getCosto());
         }
-        seleccion = lee(1, FabricaArma.catalogo.length)-1;
-        dineroInicial -=  FabricaArma.getArma(FabricaArma.catalogo[seleccion]).getCosto();
+        seleccion = lee(1, FabricaArma.catalogo.length) - 1;
+        dineroInicial.setValue(
+                dineroInicial.getValue() - FabricaArma.getArma(FabricaArma.catalogo[seleccion]).getCosto());
         String arma = FabricaArma.catalogo[seleccion];
-        
+
         System.out.println("Escoge un blindaje");
         for (int i = 0; i < FabricaBlindaje.catalogo.length; i++) {
-            System.out.println((i+1)+".-" + FabricaBlindaje.catalogo[i] +
-                    " Costo: " + FabricaBlindaje.getBlindaje(FabricaBlindaje.catalogo[i]).getCosto());
+            System.out.println((i + 1) + ".-" + FabricaBlindaje.catalogo[i] + " Costo: "
+                    + FabricaBlindaje.getBlindaje(FabricaBlindaje.catalogo[i]).getCosto());
         }
-        seleccion = lee(1, FabricaBlindaje.catalogo.length)-1;
-        dineroInicial -=  FabricaBlindaje.getBlindaje(FabricaBlindaje.catalogo[seleccion]).getCosto();
+        seleccion = lee(1, FabricaBlindaje.catalogo.length) - 1;
+        dineroInicial.setValue(
+                dineroInicial.getValue() - FabricaBlindaje.getBlindaje(FabricaBlindaje.catalogo[seleccion]).getCosto());
         String blindaje = FabricaBlindaje.catalogo[seleccion];
 
         System.out.println("Escoge un carroceria");
         for (int i = 0; i < FabricaCarroceria.catalogo.length; i++) {
-            System.out.println((i+1)+".-" + FabricaCarroceria.catalogo[i] +
-                    " Costo: " + FabricaCarroceria.getCarroceria(FabricaCarroceria.catalogo[i]).getCosto());
+            System.out.println((i + 1) + ".-" + FabricaCarroceria.catalogo[i] + " Costo: "
+                    + FabricaCarroceria.getCarroceria(FabricaCarroceria.catalogo[i]).getCosto());
         }
-        seleccion = lee(1, FabricaCarroceria.catalogo.length)-1;
-        dineroInicial -=  FabricaCarroceria.getCarroceria(FabricaCarroceria.catalogo[seleccion]).getCosto();
+        seleccion = lee(1, FabricaCarroceria.catalogo.length) - 1;
+        dineroInicial.setValue(dineroInicial.getValue()
+                - FabricaCarroceria.getCarroceria(FabricaCarroceria.catalogo[seleccion]).getCosto());
         String carroceria = FabricaCarroceria.catalogo[seleccion];
 
         System.out.println("Escoge un motor");
         for (int i = 0; i < FabricaMotor.catalogo.length; i++) {
-            System.out.println((i+1)+".-" + FabricaMotor.catalogo[i] +
-                    " Costo: " + FabricaMotor.getMotor(FabricaMotor.catalogo[i]).getCosto());
+            System.out.println((i + 1) + ".-" + FabricaMotor.catalogo[i] + " Costo: "
+                    + FabricaMotor.getMotor(FabricaMotor.catalogo[i]).getCosto());
         }
-        seleccion = lee(1, FabricaMotor.catalogo.length)-1;
-        dineroInicial -=  FabricaMotor.getMotor(FabricaMotor.catalogo[seleccion]).getCosto();
+        seleccion = lee(1, FabricaMotor.catalogo.length) - 1;
+        dineroInicial.setValue(
+                dineroInicial.getValue() - FabricaMotor.getMotor(FabricaMotor.catalogo[seleccion]).getCosto());
         String motor = FabricaMotor.catalogo[seleccion];
         return FabricaCoche.crearCoche(llanta, motor, carroceria, blindaje, arma);
-    } //Cierre método menuPersonalizado
+    } // Cierre método menuPersonalizado
 
-    private static Coche menuPredifinido(double dineroInicial){
+    private static Coche menuPredifinido(ReferenciaNumeroDoble dineroInicial) {
+        int seleccion;
         Coche[] tipos = FabricaCoche.getPredefinidos();
-        System.out.println("Tenemos "+tipos.length+" opciones para ti: ");
-        for (int i = 0; i < tipos.length; i++) {
-            System.out.println("\n"+(i+1)+":"+tipos[i].getDescripcion());
-        }
-        int seleccion = lee(1,tipos.length);
-        return tipos[seleccion-1];
+        do {
+            System.out.println("Tenemos " + tipos.length + " opciones para ti: ");
+            for (int i = 0; i < tipos.length; i++) {
+                System.out
+                        .println("\n" + (i + 1) + ":" + tipos[i].getDescripcion() + "\t Costo:" + tipos[i].getCosto());
+            }
+            seleccion = lee(1, tipos.length);
+            if (tipos[seleccion - 1].getCosto() > dineroInicial.getValue()) {
+                System.out.println("No tienes suficiente dinero, escoge otra opcion");
+                System.out.println("------------------------------------------------");
+            }
+        } while (tipos[seleccion - 1].getCosto() > dineroInicial.getValue());
+        dineroInicial.setValue(dineroInicial.getValue() - tipos[seleccion - 1].getCosto());
+        System.out.println(dineroInicial);
+        return tipos[seleccion - 1];
     }
-
 
     /**
      * Método que lee hasta que el usuario proporcione una opción válida
---
+++ Factory/src/ciencias/unam/modelado/ReferenciaNumeroDoble.java
@@ -0,0 +1,18 @@
+package ciencias.unam.modelado;
+
+public class ReferenciaNumeroDoble{
+
+    private double value;
+
+    public ReferenciaNumeroDoble(double value) {
+        this.value = value;
+    }
+
+    public double getValue() {
+        return this.value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+}
